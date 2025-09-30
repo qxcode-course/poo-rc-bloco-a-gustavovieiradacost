e57c7e3cb0c618d5adb001db5eb010ee8cf830fb
gustavo vieira <148218274+gustavovieiradacost@users.noreply.github.com>
2025-09-30 14:14:48 +0000

    corrigir codigo-toalha

--- poo/database/enxugar/py/draft.py
+++ poo/database/enxugar/py/draft.py
@@ -1,42 +1,58 @@
 class Towel:
-    def __init__(self, color: str, size: str): # construtor
-        self.color: str = color # atributos
+    def _init_(self, color: str, size: str):
+        self.color: str = color
         self.size: str = size
         self.wetness: int = 0
-    
+
     def dry(self, amount: int) -> None:
         self.wetness += amount
-        if self.wetness > self.isMaxWetness():
+        if self.wetness >= self.isMaxWetness():
             self.wetness = self.isMaxWetness()
-            print("Toalha molhada!")
+            print("toalha encharcada")
+
+    def isDry(self):
+        return self.wetness == 0
+
+    def wringOut(self):
+        self.wetness = 0
 
     def isMaxWetness(self) -> int:
-        if self.size == "P": # early return
+        if self.size == "P":
             return 10
         if self.size == "M":
             return 20
         if self.size == "G":
             return 30
-        return 0 # default return
+        return 0
 
-    def __str__(self) -> str: # toString
-        return f"Color:{self.color}, Size:{self.size}, Wet:{self.wetness}"
-    
-def main():  
-  
-    towel: Towel = Towel("", "")
+    def _str_(self) -> str:
+        return f"Cor: {self.color}, Tamanho: {self.size}, Umidade: {self.wetness}"
+
+
+def main():
+    towel: Towel = Towel("" , "")
     while True:
-        
+
         line: str = input()
+        print("$" + line)
         args: list[str] = line.split(" ")
 
         if args[0] == "end":
             break
-        elif args[0] == "new":
+        elif args[0] == "criar":
             color: str = args[1]
             size: str = args[2]
             towel = Towel(color, size)
-        elif args[0] == "show":
+        elif args[0] == "seca":
+            print("sim" if towel.isDry() else "nao")
+        elif args[0] == "torcer":
+            towel.wringOut()
+        elif args[0] == "enxugar":
+            amount: int = int(args[1])
+            towel.dry(amount)
+        elif args[0] == "mostrar":
             print(towel)
         else:
             print("fail: comando não encontrado")
+
+main()
