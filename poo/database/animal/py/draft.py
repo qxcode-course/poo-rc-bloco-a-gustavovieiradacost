5a3dd9290ebbf3a1ce8f54b5eee097305354d931
gustavo vieira <148218274+gustavovieiradacost@users.noreply.github.com>
2025-10-01 14:13:39 +0000

    animal

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,17 +1,50 @@
 class Animal:
-    def __init__(self, species, sound):
-        self.species = species 
-        self.sound = sound
-        self.age = 0
+    def __init__(self, specie: str, sound: str):
+        self.specie: str = specie
+        self.sound: str = sound
+        self.age: int = 0
 
-    def __init__(self):
-        return f"{self.species}:{self.age}:{self.sound}"
-    
-    gato = Animal("gato", "miau")
-    print(gato)
+    def __str__(self) -> str:  # toString
+        return f"{self.specie}:{self.age}:{self.sound}"
 
-    cachorro = Animal("cachorro","auau")
-    print(cachorro)
+    def ageBy(self, increment: int) -> None:
+        if self.age == 4:
+            print(f"warning: {self.specie} morreu")
+            return
+        self.age += increment
+        if self.age >= 4:
+            self.age = 4
+            print(f"warning: {self.specie} morreu")
 
-    galinha = Animal("galinha", "cocorico")
-    print("galinha")
+    def ismakeSound(self) -> str:
+        if self.age == 0:
+            return "---"
+        if self.age in (1, 2, 3):
+            return self.sound
+        if self.age == 4:
+            return "RIP"
+
+
+def main():
+    animal: Animal = Animal("", "")
+    while True:
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
+
+        if args[0] == "end":
+            break
+        if args[0] == "init":
+            specie: str = args[1]
+            sound: str = args[2]
+            animal = Animal(specie, sound)
+        if args[0] == "show":
+            print(animal)
+        if args[0] == "grow":
+            increment: int = int(args[1])
+            animal.ageBy(increment)
+        if args[0] == "noise":
+            print(animal.ismakeSound())
+
+
+main()
